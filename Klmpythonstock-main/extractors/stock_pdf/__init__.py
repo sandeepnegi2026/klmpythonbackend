ad20d7539f43e3beefd5a62dfb891851700785e4
sandeepnegi2026 <sandeep@hazemediait.com>
2026-07-17 11:12:29 +0530

    Delete Klmpythonstock-main directory

--- Klmpythonstock-main/extractors/stock_pdf/__init__.py
+++
@@ -1,5 +0,0 @@
-from extractors.stock_pdf.detect import detect_layout
-from extractors.stock_pdf.pipeline import extract
-from extractors.stock_pdf.registry import TEXT_PARSERS
-
-__all__ = ["extract", "detect_layout", "TEXT_PARSERS"]
