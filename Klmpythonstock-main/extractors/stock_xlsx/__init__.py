068fe816f42334c952200849919c3313e5adab7d
snadeep <sandeep@hazemediait.com>
2026-07-17 11:09:03 +0530

    delete all
    delete all

--- Klmpythonstock-main/extractors/stock_xlsx/__init__.py
+++
@@ -1,5 +0,0 @@
-from extractors.stock_xlsx.detect import detect_excel_layout
-from extractors.stock_xlsx.pipeline import extract
-from extractors.stock_xlsx.registry import PARSERS
-
-__all__ = ["extract", "detect_excel_layout", "PARSERS"]
